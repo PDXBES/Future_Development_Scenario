341d2b7eebe9aef5d7af84ceff1187fe728e2c7a
Dan Ashney <dashney@users.noreply.github.com>
2024-02-16 15:02:43 -0800

    Create fds_main.py
    added pseudo for starters

---
+++ fds_main.py
@@ -0,0 +1,12 @@
+
+# conditional - test if spreadsheet zones and dev cap zones are comparable
+# if no, stop and warn (get list of mismatched)
+# if yes, continue
+# get empty FdsBLiSCratch table in mem
+# append Dev cap fc to that table - map field appropriately
+# run pieces in Dave's future_base to update SCratch values
+# truncate target (2050, rename to 2070?, or is it just the BO now bc that is 2070?) and then append Scratch values to that
+# Update Future Areas (EMGAATS toolbar tool) updates additional fields in 2070/BO
+# -----keep this as run manual or incorporate into overall script?
+# -----modelers do prob always want to have this bc the model itself changing can create need for update
+# archive all inputs and the outputs (I think just the 2070/ BO now)
